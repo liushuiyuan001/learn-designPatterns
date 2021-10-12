eebf212c6e9b582580fda8873c15f53e814e28dc
Liu Shuiyuan <13783534092@sina.cn>
2021-10-12 16:10:34 +0800

    update:demo

--- design01/Demo/src/Main.java
+++ design01/Demo/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class Main {
@@ -9,7 +10,7 @@ public class Main {
 		System.out.println(b);
 		System.out.println(ch);
 		
-		int[] ns = new int[]{1,2,3,4,5,6};
+		int[] ns = new int[]{1,2,3,6,5,4};
 		System.out.println(ns[5]);
 		
 		String[] str  = new String[]{"a","b","c"};
@@ -17,7 +18,34 @@ public class Main {
 		s = "d";
 		System.out.println(str[0]);
 		
+		for (int n:	ns) {
+            System.out.println(n);
+		}
+		System.out.println("Arrays");
+		System.out.println(Arrays.toString(ns));
+		Arrays.sort(ns);
+		System.out.println(Arrays.toString(ns));
+		
 		Stream<String> stream = Stream.of("a","b","c","d","e");
 		stream.forEach(System.out::println);
+		
+		Person p = new Student();
+		p.sayHello();
+		
+	}
+}
+
+class Person {
+
+
+	 void sayHello() {
+		System.out.println("Person Hello");
+	}
+}
+
+class Student extends Person{
+
+	void sayHello(){
+	  System.out.println("Student Hello");
 	}
 }
