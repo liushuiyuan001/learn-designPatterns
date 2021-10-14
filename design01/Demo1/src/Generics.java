bb24e43e587a9df1709e812c871f7a37fcd49323
Liu Shuiyuan <13783534092@sina.cn>
2021-10-14 21:06:10 +0800

    update:demo

--- design01/Demo1/src/Generics.java
+++ design01/Demo1/src/Generics.java
@@ -3,7 +3,8 @@ import java.util.List;
 
 public class Generics {
 	public static void main(String[] args) {
-	     new Generics().go();
+//	     new Generics().go();
+		 new Generics().testE();
 	}
 	
 	public void go() {
@@ -18,6 +19,53 @@ public class Generics {
 //		dogs.add(new Dog());
 //		takeAnimals(dogs);
 		test();
+	}
+	
+	public void testE() {
+//		Integer[] intArray = new Integer[]{1,2,3,4,5};
+//		Double[] doubleArray = new Double[]{1.0,2.0,3.0,4.0,5.0};
+//		printArray(intArray,intArray);
+		
+//		System.out.println(maximum(1,2));
+//		System.out.println(maximum(new Dog(), new Dog()));
+	
+		Box<Integer> integerBox = new Box<Integer>();
+		integerBox.add(10);
+		
+		Box<String> stringBox = new Box<String>();
+		stringBox.add("Hello World");
+		
+		List<String> names = new ArrayList<String>();
+		names.add("icon");
+		getData(names);
+		
+		List<Integer> age = new ArrayList<Integer>();
+		age.add(12);
+		getData(age);
+		
+		getUperNumber(age);
+	}
+	
+	public static void getData(List<?> data){
+		System.out.println(data.get(0));
+	}
+	
+	public static  void getUperNumber(List<? extends Number> data) {
+		System.out.println(data.get(0));
+	}
+	
+	public static <E,T> void printArray(E[] inputArray,T[] otherArray) {
+	    for(E e : inputArray) {
+			System.out.println(e);
+		}
+		for(T e : otherArray) {
+			System.out.println(e);
+		}
+	}
+	
+	public static <T extends Comparable<T>> T maximum(T x,T y) {
+		
+		return x.compareTo(y) > 0 ? x : y;
 	}
 	
 	public void takeAnimals(ArrayList<? super Animal> animals) {
@@ -41,18 +89,45 @@ public class Generics {
 	}
 }
 
+class Box<T> {
+	private T t;
+	private T name;
+	
+	public void add(T t) {
+		this.t = t;
+	}
+	
+	public T getT() {
+		return t;
+	}
+	
+	public T getName() {
+		return name;
+	}
+	
+	public void setName(T name) {
+		this.name = name;
+	}
+}
 abstract class Animal {
+	public String names = "";
 	void eat(){
 		System.out.println("Animal eat");
 	}
 }
 
-class Dog extends Animal{
+class Dog extends Animal implements Comparable{
 	@Override
 	void eat(){
 		System.out.println("Dog eat");
 	}
 	void bark(){}
+	
+	@Override
+	public int compareTo(Object o) {
+		Dog g = (Dog)o;
+		return (g.names).compareTo(this.names);
+	}
 }
 
 class Cat extends Animal {
