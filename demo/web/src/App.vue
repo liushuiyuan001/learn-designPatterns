058a37a8215f8edc8b3b0cd66891d4ce7fc85e0d
Liu Shuiyuan <13783534092@sina.cn>
2022-12-18 22:46:33 +0800

    demo

---
+++ demo/web/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <a-layout>
+    <a-layout-header class="header">
+      <div class="logo" />
+      <a-menu
+        v-model:selectedKeys="selectedKeys1"
+        theme="dark"
+        mode="horizontal"
+        :style="{ lineHeight: '64px' }"
+      >
+        <a-menu-item key="1">
+          <RouterLink to="/">Home</RouterLink>
+        </a-menu-item>
+        <a-menu-item key="ebook">
+          <RouterLink to="/ebook">电子书管理</RouterLink>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <RouterLink to="/about">About</RouterLink>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-header>
+    <a-layout>
+
+      <RouterView />
+    </a-layout>
+    <a-layout-footer style="text-align: center">
+      Ant Design ©2018 Created by Ant UED
+    </a-layout-footer>
+  </a-layout>
+</template>
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { RouterLink, RouterView } from 'vue-router';
+const selectedKeys1 = ref<string[]>(['1'])
+</script>
+<style>
+.ant-layout {
+  height: 100%;
+}
+#components-layout-demo-top-side-2 .logo {
+  float: left;
+  width: 120px;
+  height: 31px;
+  margin: 16px 24px 16px 0;
+  background: rgba(255, 255, 255, 0.3);
+}
+
+.ant-row-rtl #components-layout-demo-top-side-2 .logo {
+  float: right;
+  margin: 16px 0 16px 24px;
+}
+
+.site-layout-background {
+  background: #fff;
+}
+</style>
---
+++ demo/web/src/router/index.ts
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import Ebook from '../views/Ebook.vue'
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/ebook',
+      name: 'ebook',
+      component: Ebook
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    }
+  ]
+})
+
+export default router
